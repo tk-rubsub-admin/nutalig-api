5f70f6a09f37671bf8a438e7535c582b5c7f27b9
Veerapat Prechadech <veerapat.p@evme.io>
2026-06-13 20:52:06 +0700

    println

--- src/main/java/com/nutalig/config/LineConfiguration.java
+++ src/main/java/com/nutalig/config/LineConfiguration.java
@@ -1,5 +1,6 @@
 package com.nutalig.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -101,6 +102,27 @@ public class LineConfiguration {
         private String loginFailureUrl;
         private String linkSuccessUrl;
         private String linkFailureUrl;
+    }
+
+    @PostConstruct
+    public void test() {
+        System.out.println("=== LINE CONFIG ===");
+        System.out.println("line.message.api = " + getLineMessageApiUrl());
+        System.out.println("line.channel.id = " + getLineChannelId());
+        System.out.println("line.channel.secret = " + getLineChannelSecret());
+        System.out.println("line.channel.access-token = " + getLineChannelAccessToken());
+        System.out.println("line.login.authorize-url = " + getAuthorizeUrl());
+        System.out.println("line.login.token-url = " + getTokenUrl());
+        System.out.println("line.login.verify-access-token-url = " + getVerifyAccessTokenUrl());
+        System.out.println("line.login.verify-id-token-url = " + getVerifyIdTokenUrl());
+        System.out.println("line.login.profile-url = " + getProfileUrl());
+        System.out.println("line.login.redirect-uri = " + getRedirectUri());
+        System.out.println("line.login.scope = " + getScope());
+        System.out.println("line.login.login-success-url = " + getLoginSuccessUrl());
+        System.out.println("line.login.login-failure-url = " + getLoginFailureUrl());
+        System.out.println("line.login.link-success-url = " + getLinkSuccessUrl());
+        System.out.println("line.login.link-failure-url = " + getLinkFailureUrl());
+        System.out.println("===================");
     }
 
 }
